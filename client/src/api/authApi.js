7c6c18b3f2d081c051712bbec74e0b2e720e8f0b
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-03-28 15:13:51 +0200

    Attach user profile create hook to user create

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import request from "../utils/request.js"
 import { UserContext } from "../contexts/userContext.js";
+import { useCreateUserProfile } from "./userProfileApi.js";
 
 const baseUrl = 'http://localhost:3030/users';
 
@@ -17,9 +18,11 @@ export const useLogin = () => {
 }
 
 export const useRegister = () => {
+    const {createUserProfile} = useCreateUserProfile();
     const register =  async (email, password, username) => {
-        const result = await request.post(`${baseUrl}/register`, {email, password, username, role: 'user'})
+        const result = await request.post(`${baseUrl}/register`, {email, password, username, role: 'User'})
 
+        createUserProfile(result.accessToken);
         return result;
     }
 
