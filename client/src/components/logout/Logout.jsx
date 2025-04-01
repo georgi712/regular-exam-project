e8b43dfa29cb2ff16d8941239aeb49da6dfbe00a
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-04-01 23:53:17 +0300

    Implement toast notifications to logout

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,10 +1,30 @@
 import { Navigate } from "react-router-dom";
 import { useLogout } from "../../api/authApi.js";
+import { useToastContext } from "../../contexts/ToastContext.jsx";
+import { useEffect } from "react";
 
 export default function Logout() {
-    const {isLoggedOut} = useLogout();
+    const { isLoggedOut, isLoggingOut, error } = useLogout();
+    const toast = useToastContext();
+    
+    useEffect(() => {
+        if (isLoggingOut) {
+            toast.info("Logging out...");
+        }
+        
+        if (isLoggedOut) {
+            toast.success("Successfully logged out");
+        }
+        
+        if (error) {
+            toast.error(`Logout failed: ${error}`);
+        }
+    }, [isLoggingOut, isLoggedOut, error, toast]);
   
-    return isLoggedOut
+   
+    return (isLoggedOut || error)
         ? <Navigate to="/"/>
-        : null; //TODO: Set spinner
+        : <div className="flex justify-center items-center min-h-[300px]">
+            <span className="loading loading-spinner loading-lg"></span>
+          </div>;
 }
